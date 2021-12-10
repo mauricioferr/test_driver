eecbf1b50e8e7f3572850382c83437585eccef8b
mauricioferr <70210973+mauricioferr@users.noreply.github.com>
2021-12-09 23:26:29 -0300

    Add files via upload

--- test_driver.cpp
+++ test_driver.cpp
@@ -1,25 +1,26 @@
 #include <wiringPi.h>
 
-int main(){
+int main(void){
     const int high = 1;
     const int low = 0;
 
     const int A1 = 16;
     const int A2 = 20;
     const int AEn = 13; 
-
-    pinMode (A1, OUTPUT) ;
-    pinMode (A2, OUTPUT) ;
-    pinMode (AEn, PWM_OUTPUT) ;
-
     
     const int B1 = 26;
     const int B2 = 21;
     const int BEn = 19;
+    
+    wiringPiSetup () ;
 
     pinMode (A1, OUTPUT) ;
     pinMode (A2, OUTPUT) ;
     pinMode (AEn, PWM_OUTPUT) ;
+
+    pinMode (B1, OUTPUT) ;
+    pinMode (B2, OUTPUT) ;
+    pinMode (BEn, PWM_OUTPUT) ;
 
     while (1){
         digitalWrite (A1, high) ;
