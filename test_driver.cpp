248288ab077d28f99bbc33b9933f3f7ce58cbbe7
mauricioferr <70210973+mauricioferr@users.noreply.github.com>
2021-12-09 23:20:36 -0300

    Add files via upload

--- test_driver.cpp
+++ test_driver.cpp
@@ -1,34 +1,34 @@
 #include <wiringPi.h>
 
-int main(void){
-    const int HIGH = 1;
-    const int LOW = 0;
+int main(){
+    const int high = 1;
+    const int low = 0;
 
     const int A1 = 16;
     const int A2 = 20;
     const int AEn = 13; 
 
-    void pinMode (A1, int OUTPUT) ;
-    void pinMode (A2, int OUTPUT) ;
-    void pinMode (AEn, int PWM_OUTPUT) ;
+    pinMode (A1, int OUTPUT) ;
+    pinMode (A2, int OUTPUT) ;
+    pinMode (AEn, int PWM_OUTPUT) ;
 
     
     const int B1 = 26;
     const int B2 = 21;
     const int BEn = 19;
 
-    void pinMode (A1, int OUTPUT) ;
-    void pinMode (A2, int OUTPUT) ;
-    void pinMode (AEn, int PWM_OUTPUT) ;
+    pinMode (A1, int OUTPUT) ;
+    pinMode (A2, int OUTPUT) ;
+    pinMode (AEn, int PWM_OUTPUT) ;
 
     while (1){
-        void digitalWrite (A1, HIGH) ;
-        void digitalWrite (A2, LOW) ;
-        void pwmWrite (AEn, 155) ;
+        digitalWrite (A1, high) ;
+        digitalWrite (A2, low) ;
+        pwmWrite (AEn, 155) ;
         delay(1500) ;
-        void digitalWrite (A1, LOW) ;
-        void digitalWrite (A2, HIGH) ;
-        void pwmWrite (AEn, 155) ;
+        digitalWrite (A1, low) ;
+        digitalWrite (A2, high) ;
+        pwmWrite (AEn, 155) ;
         delay(1500) ;
     }
     return 0;
